2ab61cd3164f7425afbb87ba6b41ca71721528ef
shahraiz-cb <shahraiz.ali@crowdbotics.com>
2023-03-24 01:01:05 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "platformdemo-1-0-10-39527",
-  url: "https://platformdemo-1-0-10-39527.botics.co",
-  api: "https://platformdemo-1-0-10-39527.botics.co/api/v1"
-}
+  "name": "platformdemo-1-0-10-39527",
+  "url": "https://platformdemo-1-0-10-39527.botics.co",
+  "api": "https://platformdemo-1-0-10-39527.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
---
+++ screens/Untitled1/index.js
@@ -0,0 +1,35 @@
+import { Text } from "react-native";
+import React from "react";
+import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled1 = () => {
+  return <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={{
+      backgroundColor: '#f0f0f1',
+      padding: 10,
+      position: 'relative',
+      flex: 1
+    }}><View style={styles.gPJJRprh}><Text style={styles.LaXfSBAb}>Lorem ipsum…</Text></View></ScrollView>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  },
+  gPJJRprh: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  },
+  LaXfSBAb: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  }
+});
+export default Untitled1;
